7bd979a74c463f12106071fd2c84dc52379c0b66
mohdhamzakhan <mohdhamzakhan@gmail.com>
2016-06-03 19:14:41 +0530

    Add files via upload

---
+++ Assignment4.java
@@ -0,0 +1,13 @@
+package session1;
+
+public class Assignment4 {
+public static void main(String[] args)
+{
+	int x=5,y=10;
+	
+	System.out.println(x+y*2);
+	System.out.println(x-y*2);
+	System.out.println((x+y)*2);
+	System.out.println(y%x);
+}
+}
